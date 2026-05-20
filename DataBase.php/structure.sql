2e50ba2d257c90ddb3a30af5cbea052969fb30a2
Abdellah <hibazekraoui23@email.com>
2026-05-20 10:54:04 +0100

    created the db structure and the ERD

---
+++ DataBase.php/structure.sql
@@ -0,0 +1,96 @@
+-- Active: 1779270511082@@127.0.0.1@3306
+CREATE DATABASE peersync ;
+use peersync ;
+
+CREATE TABLE `roles` (
+  `id` INT NOT NULL,
+  `role` VARCHAR(50),
+  PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `users` (
+  `id` INT AUTO_INCREMENT,
+  `name` VARCHAR(50)  NOT NULL,
+  `email` VARCHAR(100)  NOT NULL,
+  `password` VARCHAR(255)  NOT NULL,
+  `role_id` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (`role_id`)
+      REFERENCES `roles`(`id`)
+);
+
+CREATE TABLE `skills` (
+  `id` INT AUTO_INCREMENT,
+  `name` VARCHAR(200),
+  `dificulti` ENUM('easy', 'medium',"hard") NOT NULL,
+  PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `user_skills` (
+  `id` INT AUTO_INCREMENT,
+  `maitrise` ENUM("maîtrisées","à travailler") NOT NULL,
+  `skill_id` INT NOT NULL,
+  `user_id` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (`skill_id`)
+      REFERENCES `skills`(`id`),
+  FOREIGN KEY (`user_id`)
+      REFERENCES `users`(`id`)
+);
+
+CREATE TABLE `badges` (
+  `id` INT AUTO_INCREMENT,
+  `requirements` INT NOT NULL,
+  `name` VARCHAR(50)  NOT NULL,
+  PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `user_badges` (
+  `id` INT AUTO_INCREMENT,
+  `user_id` INT NOT NULL,
+  `skill_id` INT NOT NULL,
+  `badge_id` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (`skill_id`)
+      REFERENCES `skills`(`id`),
+  FOREIGN KEY (`user_id`)
+      REFERENCES `users`(`id`),
+  FOREIGN KEY (`badge_id`)
+      REFERENCES `badges`(`id`)
+);
+
+CREATE TABLE `reviews` (
+  `id` INT AUTO_INCREMENT,
+  `comment` TEXT,
+  `rating` INT,
+  `help_quest_id` INT NOT NULL,
+  `reviewer_id` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (`reviewer_id`)
+      REFERENCES `users`(`id`)
+);
+
+CREATE TABLE `status` (
+  `id` INT NOT NULL,
+  `status` VARCHAR(50),
+  PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `help_requests` (
+  `id` INT AUTO_INCREMENT,
+  `title` VARCHAR(255),
+  `description` TEXT,
+  `learner_id` INT NOT NULL,
+  `tutor_id` INT,
+  `skill_id` INT NOT NULL,
+  `status_id` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (`tutor_id`)
+      REFERENCES `users`(`id`),
+  FOREIGN KEY (`learner_id`)
+      REFERENCES `users`(`id`),
+  FOREIGN KEY (`skill_id`)
+      REFERENCES `skills`(`id`),
+  FOREIGN KEY (`status_id`)
+      REFERENCES `status`(`id`)
+);
